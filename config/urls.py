694c2ab1cf06fa06bdf2533327fc47c540a4c178
Park <angle307@naver.com>
2023-05-10 13:39:03 +0900

    [ADD] base templates

--- config/urls.py
+++ config/urls.py
@@ -17,7 +17,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name="index"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
