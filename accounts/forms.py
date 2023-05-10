4d81bd2d8f26198cbe60b49cb08ba55e514a3d1e
armadillo <dltngks54@gmail.com>
2023-05-10 17:21:54 +0900

    [ADD]accounts:프로필, 정보수정, 탈퇴, 팔로우

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
 from django.contrib.auth import get_user_model
 
 #회원가입
@@ -13,3 +13,11 @@ class CustomUserChangeForm(UserChangeForm):
         model = get_user_model()
         fields = ('email','first_name','last_name', 'birthday', 'profile_image',)
 
+# 비밀번호 수정
+class CustomPasswordChangeForm(PasswordChangeForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['old_password'].widget.attrs['placeholder'] = '현재 비밀번호를 입력해 주세요'
+        self.fields['new_password1'].widget.attrs['placeholder'] = '새 비밀번호를 입력해 주세요'
+        self.fields['new_password2'].widget.attrs['placeholder'] = '새 비밀번호를 다시 입력해 주세요'
--- accounts/urls.py
+++ accounts/urls.py
@@ -3,12 +3,12 @@ from . import views
 
 app_name = 'accounts'
 urlpatterns = [
-    # path('login/', views.login, name='login'),
-    # path('logout/', views.logout, name='logout'),
     path('signup/', views.Signup.as_view(), name='signup'),
-    # path('delete/', views.delete, name='delete'),
-    # path('update/', views.update, name='update'),
-    # path('password/', views.change_password, name='change_password'),
-    # path('profile/<str:username>/', views.profile, name='profile'),
-    # path('<int:user_pk>/follow/', views.follow, name='follow'),
+    path('login/', views.Login.as_view(), name='login'),
+    path('logout/', views.logout, name='logout'),
+    path('profile/<str:username>/', views.profile, name='profile'),
+    path('update/', views.ProfileUpdate.as_view(), name='update'),
+    path('password/', views.ChangePassword.as_view(), name='change_password'),
+    path('delete/', views.delete, name='delete'),
+    path('<int:user_pk>/follow/', views.follow, name='follow'),
 ]
