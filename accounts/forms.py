809635e8e4cc0276006b0f18f6dd1945e199f9f4
armadillo <dltngks54@gmail.com>
2023-05-10 14:27:53 +0900

    [ADD]accounts:로그인/회원가입/로그아웃

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,10 +5,11 @@ from django.contrib.auth import get_user_model
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
+        fields = ('username', 'email', 'first_name', 'last_name', 'password1', 'password2', 'profile_image',)
 
 #회원정보 수정
 class CustomUserChangeForm(UserChangeForm):
     class Meta(UserChangeForm.Meta):
         model = get_user_model()
-        fields = ('email','first_name','last_name',)
+        fields = ('email','first_name','last_name', 'birthday', 'profile_image',)
 
