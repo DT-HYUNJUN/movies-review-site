71318632af55a24919ff41bdb4fecd5504655f5a
syyoung <yuyoung.dev@gmail.com>
2023-05-11 10:03:13 +0900

    [ADD] account-form update

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,13 +5,35 @@ from django.contrib.auth import get_user_model
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
-        fields = ('username', 'email', 'first_name', 'last_name', 'password1', 'password2', 'profile_image',)
+        fields = ('username', 'email', 'password1', 'password2', 'profile_image',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs['placeholder'] = '아이디를 입력해 주세요'
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['email'].widget.attrs['placeholder'] = '이메일을 입력해 주세요'
+        self.fields['email'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['placeholder'] = '비밀번호를 입력해 주세요'
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['placeholder'] = '비밀번호 다시 입력해 주세요'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
+        self.fields['profile_image'].widget.attrs['class'] = 'form-control'
+
+
 
 #회원정보 수정
 class CustomUserChangeForm(UserChangeForm):
     class Meta(UserChangeForm.Meta):
         model = get_user_model()
-        fields = ('email','first_name','last_name', 'birthday', 'profile_image',)
+        fields = ('email', 'birthday', 'profile_image',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['email'].widget.attrs['class'] = 'form-control'
+        self.fields['birthday'].widget.attrs['class'] = 'form-control'
+        self.fields['profile_image'].widget.attrs['class'] = 'form-control'
+
 
 # 비밀번호 수정
 class CustomPasswordChangeForm(PasswordChangeForm):
@@ -19,5 +41,8 @@ class CustomPasswordChangeForm(PasswordChangeForm):
         super().__init__(*args, **kwargs)
 
         self.fields['old_password'].widget.attrs['placeholder'] = '현재 비밀번호를 입력해 주세요'
+        self.fields['old_password'].widget.attrs['class'] = 'form-control'
         self.fields['new_password1'].widget.attrs['placeholder'] = '새 비밀번호를 입력해 주세요'
+        self.fields['new_password1'].widget.attrs['class'] = 'form-control'
         self.fields['new_password2'].widget.attrs['placeholder'] = '새 비밀번호를 다시 입력해 주세요'
+        self.fields['new_password2'].widget.attrs['class'] = 'form-control'
