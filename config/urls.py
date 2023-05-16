2b78993f41223464301452dc1d41cc9f2797bf99
armadillo <dltngks54@gmail.com>
2023-05-16 18:17:51 +0900

    [FIX]오류발생(속도느림)

--- config/urls.py
+++ config/urls.py
@@ -13,10 +13,3 @@ urlpatterns = [
     path('reviews/', include('reviews.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-if settings.DEBUG:
-    import debug_toolbar
-    urlpatterns += [
-        path('__debug__/', include(debug_toolbar.urls)),
-    ]
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root = settings.MEDIA_ROOT)
